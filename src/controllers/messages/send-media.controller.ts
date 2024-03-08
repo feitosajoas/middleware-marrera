8bd4473a8a74d61e136c48db326a4f58d742978b
Joás Feitosa <joasfeitosadelimasilva@gmail.com>
2024-03-08 11:05:04 -0300

    Ajuste tipagem e swagger

--- src/controllers/messages/send-media.controller.ts
+++ src/controllers/messages/send-media.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { SendMediaService } from 'src/business/messages/send-media.service';
+import { SendMediaRequest } from 'src/models/send-media-request';
 
 @ApiTags('Messages')
 @Controller()
@@ -10,7 +11,7 @@ export class SendMediaController {
   @Post('send-media/id=:instanceId')
   async sendMessage(
     @Param('instanceId') instanceId: string,
-    @Body() payload: any,
+    @Body() payload: SendMediaRequest,
   ) {
     // console.log(instanceId);
     const response = await this.messageService.sendMedia(instanceId, payload);
--- src/controllers/messages/send-message-text.controller.ts
+++ src/controllers/messages/send-message-text.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { SendMessageRequest } from 'src/models/send-message-request';
 import { SendMessageTextService } from './../../business/messages/send-message-text.service';
 
 @ApiTags('Messages')
@@ -10,7 +11,7 @@ export class SendMessageTextController {
   @Post('send-message/id=:instanceId')
   async sendMessage(
     @Param('instanceId') instanceId: string,
-    @Body() payload: any,
+    @Body() payload: SendMessageRequest,
   ) {
     // console.log(instanceId);
     const response = await this.messageService.sendMessage(instanceId, payload);
